66fd1a73f81c993a0e040ed5a0fa0c5d05e71f53
Gi Wah Davalos <giwirodavalos@gmail.com>
2018-08-19 04:16:57 -0500

    added more control over methods and routing

--- controller.c
+++ controller.c
@@ -1,15 +1,44 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 #include "microhttpd.h"
 #include "logger.h"
 #include "controller.h"
+
+int method_not_allowed(struct MHD_Connection *connection) {
+    int ret;
+    struct MHD_Response *response;
+    response = MHD_create_response_from_buffer(0, "", MHD_RESPMEM_PERSISTENT);
+    ret = MHD_queue_response(connection, MHD_HTTP_METHOD_NOT_ALLOWED, response);
+    MHD_destroy_response(response);
+    return ret;
+}
+
+int not_found(struct MHD_Connection *connection) {
+    int ret;
+    struct MHD_Response *response;
+    response = MHD_create_response_from_buffer(0, "", MHD_RESPMEM_PERSISTENT);
+    ret = MHD_queue_response(connection, MHD_HTTP_NOT_FOUND, response);
+    MHD_destroy_response(response);
+    return ret;
+}
+
 
 int answer_to_connection(void *cls, struct MHD_Connection *connection,
                          const char *url, const char *method,
                          const char *version, const char *upload_data,
                          size_t *upload_data_size, void **con_cls)
 {
-  printf ("[%s], New %s request for %s using version %s\n", hostname, method, url, version);
+    // Methods that are not allowed
+    if (strcmp(method, MHD_HTTP_METHOD_GET) != 0
+        && strcmp(method, MHD_HTTP_METHOD_OPTIONS) != 0) {
+        return method_not_allowed(connection);
+    }
+    // Route check
+    if (strcmp(url, URL) != 0) {
+        return not_found(connection);
+    }
+    printf ("[%s], New %s request for route: %s using version %s\n", hostname, method, url, version);
 
-  return MHD_NO;
+    return MHD_NO;
 }
--- controller.h
+++ controller.h
@@ -1,6 +1,9 @@
 #ifndef _CONTROLLER_H
 #define _CONTROLLER_H
 
+#define URL "/whois"
+#define URL_SIZE 6
+
 // This little fucker here is a hack, because we ensure the hostname variable
 // existance in the main.c file, we put this in order to compile with no not found errors.
 char *hostname;
